5a2696207ea2727b291b63867dc5d7577ad2cea4
Anastasia-Chuvakova <nansis92@gmail.com>
2022-07-13 20:07:07 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { React } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  //initializing the apollo client
+  const client = new ApolloClient({
+    uri: "https://flyby-gateway.herokuapp.com/",//specifies the URL of our GraphQL server.
+    cache: new InMemoryCache() //the cache is an instance of the InMemoryCache, 
+    //which Apollo Client uses to cache query results after fetching them.
+  });
+  //our client is now ready to start fetching data.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  
+//connect Apollo Client to React with the ApolloProvider component
+  root.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  )
